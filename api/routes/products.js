b940da78640a2bb2074c9bf44eb70ee4e3545371
Samuel O. Asare <asare11samuel@gmail.com>
2022-11-09 15:11:06 +0000

    feat: created routes for products

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,9 +7,27 @@ router.get("/", (req, res, next)=>{
     })
 })
 
+router.get("/:productId", (req, res, next)=>{
+
+    const id = req.params.productId
+    
+    res.status(200).json({
+        message: `handling GET requests for product with id: ${id}`
+    })
+})
+
 router.post("/", (req, res, next)=>{
     res.status(200).json({
         message: "handling POST requests to /products"
+    })
+})
+
+router.patch("/:productId", (req, res, next)=>{
+
+    const id = req.params.productId
+    
+    res.status(200).json({
+        message: `updated product with id: ${id}`
     })
 })
 
