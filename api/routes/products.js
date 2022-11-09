602639bdc1c5d16c9e7f181ceeb3716adaa0137d
Samuel O. Asare <asare11samuel@gmail.com>
2022-11-09 15:00:34 +0000

    initial commit

---
+++ api/routes/products.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res, next)=>{
+    res.status(200).json({
+        message: "handling GET requests to /products"
+    })
+})
+
+router.post("/", (req, res, next)=>{
+    res.status(200).json({
+        message: "handling POST requests to /products"
+    })
+})
+
+module.exports = router
---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+const productsRoutes = require("./api/routes/products")
+
+app.use("/products", productsRoutes)
+
+
+module.exports = app
